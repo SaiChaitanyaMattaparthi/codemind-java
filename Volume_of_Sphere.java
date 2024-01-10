a40c9d94a85f1c67ed12154551101849825ee671
SaiChaitanyaMattaparthi <119723737+SaiChaitanyaMattaparthi@users.noreply.github.com>
2024-01-10 11:07:29 +0530

    import java.util.Scanner; public class a{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int a=s.nextInt();         System.out.printf("%.2f",(4.0/3.0)*(3.14*a*a*a));     } }

---
+++ Volume_of_Sphere.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class a{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        System.out.printf("%.2f",(4.0/3.0)*(3.14*a*a*a));
+    }
+}
