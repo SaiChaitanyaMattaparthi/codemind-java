24918de969c8b9d79f2115fc5045cebaac53b0e0
SaiChaitanyaMattaparthi <119723737+SaiChaitanyaMattaparthi@users.noreply.github.com>
2024-02-12 20:55:48 +0530

    import java.util.Scanner; public class a{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         if(a<=98){             System.out.println("NO");         }         else{             System.out.println("YES");         }     } }

---
+++ Fever.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class a{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a<=98){
+            System.out.println("NO");
+        }
+        else{
+            System.out.println("YES");
+        }
+    }
+}
